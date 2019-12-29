b8dea4ccddef8f9fff53518e8a74ccb21f0efba0
sdrozd97 <sebastian.drozd97@gmail.com>
2019-12-29 18:44:41 +0100

    clean project files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar />
+    <h1>Test</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '@/components/Navbar.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
