803a7cffa1c142021c3a231f8e17e3519ac7eeb0
sdrozd97 <sebastian.drozd97@gmail.com>
2020-02-19 15:14:21 +0100

    feature: search events by date

--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="event-list">
     <div class="list-group">
-      <input
-        v-model="searchText"
-        class="list-group-item search"
-        type="text"
-        placeholder="Search..."
-      />
+      <div class="search-bar">
+        <input
+          v-model="searchText"
+          class="list-group-item search"
+          type="text"
+          placeholder="Search..."
+        />
+        <input
+          v-model="searchDate"
+          class="list-group-item search"
+          type="text"
+          placeholder="Date"
+          onfocus="(this.type='date')"
+          onblur="(this.type='text')"
+        />
+      </div>
+
       <Event v-for="(event, index) in filteredEvents" :key="index" :event="event" />
     </div>
   </div>
@@ -25,15 +36,30 @@ export default {
     return {
       event,
       searchText: '',
+      searchDate: '',
     };
   },
   computed: {
     filteredEvents: function() {
       return this.events.filter(event => {
-        return event.title
-          .toLowerCase()
-          .trim()
-          .match(this.searchText.toLowerCase().trim());
+        if (this.searchDate) {
+          const dateFrom = new Date(event.dateFrom);
+          const dateTo = new Date(event.dateTo);
+          const date = new Date(this.searchDate);
+          return (
+            event.title
+              .toLowerCase()
+              .trim()
+              .match(this.searchText.toLowerCase().trim()) &&
+            date.getTime() <= dateTo.getTime() &&
+            date.getTime() >= dateFrom.getTime()
+          );
+        } else {
+          return event.title
+            .toLowerCase()
+            .trim()
+            .match(this.searchText.toLowerCase().trim());
+        }
       });
     },
   },
@@ -60,11 +86,26 @@ export default {
   }
 }
 
-.search {
-  background: #ebebeb;
+.search-bar {
+  width: 100%;
+  height: 8vh;
+  box-sizing: border-box;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+  display: flex;
 
-  &:focus {
-    outline: none;
+  .search {
+    width: 60%;
+    border: none;
+    background-color: #ebebeb;
+    height: 100%;
+    &:focus {
+      outline: none;
+    }
+
+    &:last-child {
+      width: 40%;
+      border-left: 1px solid rgba(0, 0, 0, 0.15);
+    }
   }
 }
 </style>
